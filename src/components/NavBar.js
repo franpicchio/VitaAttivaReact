154cf6055960464435dc1e15992ec0d9ae8aa64b
franpicchio <fjpicchioluque@gmail.com>
2021-11-30 23:51:30 -0300

    Desafio Clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavBar = () => {
+    return(
+        <div>
+            <h1>Vita Attiva</h1>
+            <ul>
+                <li>
+                    <a>Inicio</a>
+                </li>
+                <li>
+                    <a>Productos</a>
+                </li>
+                <li>
+                    <a>Contacto</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
