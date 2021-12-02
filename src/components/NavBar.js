21a8bec2036c572e81574ca9bdd7bfd95e9c21fe
franpicchio <fjpicchioluque@gmail.com>
2021-12-02 11:20:09 -0300

    Desafío Clase 3 bien hecho

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
 const NavBar = () => {
-    return(
+    return (
         <div>
             <h1>Vita Attiva</h1>
-            <ul>
-                <li>
-                    <a>Inicio</a>
-                </li>
-                <li>
-                    <a>Productos</a>
-                </li>
-                <li>
-                    <a>Contacto</a>
-                </li>
-            </ul>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.html">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="productos.html">Productos</a>
+                    </li>
+                    <li>
+                        <a href="contacto.html">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
