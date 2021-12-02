c9aac985d75094ca2bba853955e1e4bd78058f14
franpicchio <fjpicchioluque@gmail.com>
2021-12-02 11:37:29 -0300

    Nav Bar con Bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
         <div>
-            <h1>Vita Attiva</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="index.html">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="productos.html">Productos</a>
-                    </li>
-                    <li>
-                        <a href="contacto.html">Contacto</a>
-                    </li>
-                </ul>
-            </nav>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="index.html">Vita Attiva</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="index.html">Inicio</Nav.Link>
+                        <NavDropdown title="Productos" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="suplemetos.html">Suplementos Deportivos</NavDropdown.Item>
+                        <NavDropdown.Item href="frutosSecos.html">Frutos Secos</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="contacto.html">Contacto</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
