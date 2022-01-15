5ae4640efd566a92a3b4753f3f091df7a86c2549
franpicchio <fjpicchioluque@gmail.com>
2022-01-15 16:45:22 -0300

    Desafío clase 13 correcto

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,49 @@
 import React from 'react'
+import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { useCartContext } from '../context/CartContext'
 import { useCompraContext } from '../context/CompraContext'
 import { InfoComprador } from './InfoComprador'
+import {collection, addDoc, getFirestore} from 'firebase/firestore'
 
 function Cart() {
     const {cartList, BorrarCarrito,BorrarItem, totalCompra} = useCartContext()
-    const {finCompra, FinalizarCompra} = useCompraContext()
+    const [idOrden, setIdOrden] = useState('')
+    const [finCompra, setFinCompra] = useState(false)
+
+    const FinalizarCompra = (e) =>{
+
+        e.preventDefault()
+        let orden = {}
+        orden.comprador = {nombre: 'Francisco', mail: 'francisco@gmail.com', telefono:'1234567890'}
+        orden.total = totalCompra()
+
+        orden.items = cartList.map(cartItem => {
+            const id = cartItem.id
+            const nombre = cartItem.nombre
+            const precio = cartItem.precio
+            const cantidad = cartItem.cantidad
+
+            return {id, nombre, precio, cantidad}
+        })
+
+        const bd = getFirestore()
+        const ordenColeccion = collection(bd, 'ordenes')
+        addDoc(ordenColeccion, orden)
+        .then(resp => setIdOrden(resp.id))
+        .catch(err => console.log(err))
+        .finally(()=> setFinCompra(false))
+
+        console.log(orden)
+        console.log(idOrden)
+    }
 
     return (
-            <section>
-            {finCompra ?
-            <InfoComprador />
-                :
-                
+    <section>
+        {idOrden !== '' ?
+            <h2>El id de su orden es {idOrden}</h2>
+            :
+            
                 <>
                         <table class="table">
                             <thead>
@@ -40,11 +70,14 @@ function Cart() {
                                 <th scope="row">Total compra = ${totalCompra()}</th>
                             </tr>
                         </table>
-                    <button className='btn btn-success mx-2' onClick={()=>FinalizarCompra()}>Continuar con la Compra</button>
+                    <form onSubmit={FinalizarCompra}>
+                        <button className='btn btn-success mx-2'>Continuar con la Compra</button>
+                    </form>
                     <button className='btn btn-danger' onClick={BorrarCarrito}>Vaciar Carrito</button>
                 </>
-                }
-            </section>
+            
+        }
+    </section>
     )
 }
 
--- src/context/CompraContext.js
+++ src/context/CompraContext.js
@@ -1,14 +1,14 @@
 import { createContext, useContext, useState } from "react";
 import { useCartContext } from "./CartContext";
-import {collection, addDoc, getFirestore} from 'firebase/firestore'
+
 
 const CompraContext = createContext([])
 
 export const useCompraContext = () => useContext(CompraContext)
 
 export const CompraContextProvider = ({children}) => {
-    const [finCompra, setFinCompra] = useState(false)
-    const [idOrden, setIdOrden] = useState('')
+
+    
     const [ordenComprador, setOrdenComprador] = useState({nombre:"", email:"", telefono:""})
 
     const {totalCompra, cartList} = useCartContext()
@@ -22,38 +22,13 @@ export const CompraContextProvider = ({children}) => {
         })*/
     }
 
-    console.log(ordenComprador)
+    //console.log(ordenComprador)
 
-    const FinalizarCompra = (e) =>{
-        e.preventDefault()
-
-        let orden = {}
-
-        orden.comprador = ordenComprador
-        orden.total = totalCompra()
-
-        orden.items = cartList.map(cartItem => {
-            const id = cartItem.id
-            const nombre = cartItem.nombre
-            const precio = cartItem.precio
-            const cantidad = cartItem.cantidad
-
-            return {id, nombre, precio, cantidad}
-        })
-
-        const bd = getFirestore()
-        const ordenColeccion = collection(bd, 'ordenes')
-        addDoc(ordenColeccion, orden)
-        .then(resp => setIdOrden(resp.id))
-        .catch(err => console.log(err))
-        .finally(()=> setFinCompra(false))
-
-        console.log(orden)
-    }
+    
 
 
     return (
-            <CompraContext.Provider value={{finCompra, idOrden, setOrdenComprador, FinalizarCompra, HandleOrden}}>
+            <CompraContext.Provider value={{setOrdenComprador, /*FinalizarCompra,*/ HandleOrden}}>
                 {children}
             </CompraContext.Provider>
     )
